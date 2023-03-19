830444b39159fb8f11113a2c36fdccdea4027364
angel-zepeda <angel.zepeda0508@gmail.com>
2023-03-19 16:53:23 -0600

    feat: add banner

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -3,53 +3,55 @@ import Image from "next/image";
 
 const Signin = () => {
   return (
-    <div className="min-h-screen w-full flex items-start justify-center">
-      <div>
-        <Image
-          src={"/logo.png"}
-          width={300}
-          height={300}
-          alt="TCG Tournament Tracker"
-        />
-        <section className="bg-emerald-500 rounded-lg">
-          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-emerald-500">
-                  Sign in to your account
-                </h1>
-                <form className="space-y-4 md:space-y-6" action="#">
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block mb-2 text-sm font-medium text-dark1 "
-                    >
-                      Your email
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      className="focus:outline-none border-b rounded-none w-full pb-2 border-gray-400 placeholder-gray-500 bg-white"
-                      placeholder="name@company.com"
-                    />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="block mb-2 text-sm font-medium text-dark1"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      className="focus:outline-none border-b rounded-none w-full pb-2 border-gray-400 placeholder-gray-500 bg-white"
-                    />
-                  </div>
-                  {/* <div className="flex items-center justify-between">
+    <>
+      <div className="absolute w-full top-0 bg-emerald-400 h-40 -z-10 banner-header"></div>
+      <div className="min-h-screen w-full flex items-start justify-center pt-10">
+        <div>
+          <Image
+            src={"/logo.png"}
+            width={300}
+            height={300}
+            alt="TCG Tournament Tracker"
+          />
+          <section className="bg-emerald-500 rounded-lg">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+              <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-emerald-500">
+                    Sign in to your account
+                  </h1>
+                  <form className="space-y-4 md:space-y-6" action="#">
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-dark1 "
+                      >
+                        Your email
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="focus:outline-none border-b rounded-none w-full pb-2 border-gray-400 placeholder-gray-500 bg-white"
+                        placeholder="name@company.com"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-dark1"
+                      >
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        className="focus:outline-none border-b rounded-none w-full pb-2 border-gray-400 placeholder-gray-500 bg-white"
+                      />
+                    </div>
+                    {/* <div className="flex items-center justify-between">
                   <div className="flex items-start">
                     <div className="flex items-center h-5">
                       <input
@@ -75,25 +77,26 @@ const Signin = () => {
                     Forgot password?
                   </a>
                 </div> */}
-                  <button className="w-full text-white bg-emerald-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    Sign in
-                  </button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                    Don’t have an account yet?{" "}
-                    <Link
-                      href="/signup"
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    >
-                      Sign up
-                    </Link>
-                  </p>
-                </form>
+                    <button className="w-full text-white bg-emerald-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                      Sign in
+                    </button>
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet?{" "}
+                      <Link
+                        href="/signup"
+                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                      >
+                        Sign up
+                      </Link>
+                    </p>
+                  </form>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default Signin;
