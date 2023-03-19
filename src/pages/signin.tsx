eee2712ed387a1594acccfca9cdca8a28e1a944e
angel-zepeda <angel.zepeda0508@gmail.com>
2023-03-19 17:05:37 -0600

    style: change logo

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -4,14 +4,23 @@ import Image from "next/image";
 const Signin = () => {
   return (
     <>
-      <div className="absolute w-full top-0 bg-emerald-400 h-40 -z-10 banner-header"></div>
+      <div className="absolute w-full top-0  h-40 -z-10">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#11b981"
+            fill-opacity="1.4"
+            d="M0,32L16,53.3C32,75,64,117,96,122.7C128,128,160,96,192,117.3C224,139,256,213,288,224C320,235,352,181,384,186.7C416,192,448,256,480,266.7C512,277,544,235,576,186.7C608,139,640,85,672,85.3C704,85,736,139,768,160C800,181,832,171,864,186.7C896,203,928,245,960,261.3C992,277,1024,267,1056,245.3C1088,224,1120,192,1152,192C1184,192,1216,224,1248,224C1280,224,1312,192,1344,202.7C1376,213,1408,267,1424,293.3L1440,320L1440,0L1424,0C1408,0,1376,0,1344,0C1312,0,1280,0,1248,0C1216,0,1184,0,1152,0C1120,0,1088,0,1056,0C1024,0,992,0,960,0C928,0,896,0,864,0C832,0,800,0,768,0C736,0,704,0,672,0C640,0,608,0,576,0C544,0,512,0,480,0C448,0,416,0,384,0C352,0,320,0,288,0C256,0,224,0,192,0C160,0,128,0,96,0C64,0,32,0,16,0L0,0Z"
+          ></path>
+        </svg>
+      </div>
       <div className="min-h-screen w-full flex items-start justify-center pt-10">
         <div>
           <Image
             src={"/logo.png"}
-            width={300}
-            height={300}
+            width={200}
+            height={200}
             alt="TCG Tournament Tracker"
+            className="m-auto"
           />
           <section className="bg-emerald-500 rounded-lg">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
